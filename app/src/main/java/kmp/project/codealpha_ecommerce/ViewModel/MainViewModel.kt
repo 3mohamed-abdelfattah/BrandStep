11890dcf8881e9b7d8b3faac96f9640d6929c8d8
MohamedMohamed <m01126165016@icloud.com>
2024-07-05 23:50:44 +0300

    Main View Model (Load Brand Function)

--- app/src/main/java/kmp/project/codealpha_ecommerce/ViewModel/MainViewModel.kt
+++ app/src/main/java/kmp/project/codealpha_ecommerce/ViewModel/MainViewModel.kt
@@ -7,6 +7,7 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import kmp.project.codealpha_ecommerce.Model.BrandModel
 import kmp.project.codealpha_ecommerce.Model.SliderModel
 
 class MainViewModel : ViewModel() {
@@ -14,7 +15,9 @@ class MainViewModel : ViewModel() {
     private val firebaseDatabase = FirebaseDatabase.getInstance()
 
     private val _banner = MutableLiveData<List<SliderModel>>()
+    private val _brand = MutableLiveData<MutableList<BrandModel>>()
 
+    val brands: LiveData<MutableList<BrandModel>> = _brand
     val banner: LiveData<List<SliderModel>> = _banner
 
     fun loadBanner() {
@@ -31,11 +34,26 @@ class MainViewModel : ViewModel() {
                 _banner.value = lists
             }
 
+            override fun onCancelled(error: DatabaseError) {}
+        })
+    }
 
-            override fun onCancelled(error: DatabaseError) {
 
+    fun loadBrand() {
+        val ref = firebaseDatabase.getReference("Category")
+        ref.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val lists = mutableListOf<BrandModel>()
+                for (childSnapShot in snapshot.children) {
+                    val list = childSnapShot.getValue(BrandModel::class.java)
+                    if (list != null) {
+                        lists.add(list)
+                    }
+                }
+                _brand.value = lists
             }
 
+            override fun onCancelled(error: DatabaseError) {}
         })
     }
 }
