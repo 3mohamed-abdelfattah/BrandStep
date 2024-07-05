3fa147ae7e89b82b4275f2e60ce2963583bd6787
MohamedMohamed <m01126165016@icloud.com>
2024-07-05 03:39:29 +0300

    Dashboard Activity UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,4 +75,6 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation("com.tbuonomo:dotsindicator:5.0")
+
 }
