f37a05f11642541d8dd19ea50d56b1af899b785e
MohamedMohamed <m01126165016@icloud.com>
2024-07-06 12:23:03 +0300

    Popular Adapter

---
+++ app/src/main/java/kmp/project/codealpha_ecommerce/Adapter/PopularAdapter.kt
@@ -0,0 +1,46 @@
+package kmp.project.codealpha_ecommerce.Adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.request.RequestOptions
+import kmp.project.codealpha_ecommerce.Model.ItemModel
+import kmp.project.codealpha_ecommerce.databinding.ViewHolderRecommendedBinding
+
+class PopularAdapter(val items: MutableList<ItemModel>) :
+    RecyclerView.Adapter<PopularAdapter.ViewHolder>() {
+
+    private var context: Context? = null
+
+    class ViewHolder(val binding: ViewHolderRecommendedBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PopularAdapter.ViewHolder {
+        context = parent.context
+        val binding =
+            ViewHolderRecommendedBinding.inflate(LayoutInflater.from(context), parent, false)
+        return ViewHolder(binding)
+    }
+
+
+    override fun onBindViewHolder(holder: PopularAdapter.ViewHolder, position: Int) {
+        holder.binding.titleText.text = items[position].title
+        holder.binding.priceTxt.text = "$" + items[position].price.toString()
+        holder.binding.ratingText.text = items[position].rating.toString()
+
+        val requestOptions = RequestOptions().transform(CenterCrop())
+        Glide.with(holder.itemView.context)
+            .load(items[position].picUrl[0])
+            .apply(requestOptions)
+            .into(holder.binding.pic)
+
+//        holder.itemView.setOnClickListener {
+//            val intent = Intent(holder.itemView.context)
+//        }
+    }
+
+    override fun getItemCount(): Int = items.size
+}
