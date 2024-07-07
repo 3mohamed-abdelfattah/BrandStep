490c337f7f708178e5e67f1d6c1f6d98cc325df1
MohamedMohamed <m01126165016@icloud.com>
2024-07-07 12:36:20 +0300

    Items Model

---
+++ app/src/main/java/kmp/project/codealpha_ecommerce/Model/ItemsModel.kt
@@ -0,0 +1,47 @@
+package kmp.project.codealpha_ecommerce.Model
+
+import android.os.Parcel
+import android.os.Parcelable
+
+data class ItemsModel(
+    var title: String = "",
+    var description: String = "",
+    var picUrl: ArrayList<String> = ArrayList(),
+    var size: ArrayList<String> = ArrayList(),
+    var price: Double = 0.0,
+    var rating: Double = 0.0,
+    var numberInCart: Int = 0
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString().toString(),
+        parcel.readString().toString(),
+        parcel.createStringArrayList() as ArrayList<String>,
+        parcel.createStringArrayList() as ArrayList<String>,
+        parcel.readDouble(),
+        parcel.readDouble(),
+    )
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    override fun writeToParcel(p0: Parcel, p1: Int) {
+        p0.writeString(title)
+        p0.writeString(description)
+        p0.writeStringList(picUrl)
+        p0.writeStringList(size)
+        p0.writeDouble(price)
+        p0.writeDouble(rating)
+
+    }
+
+    companion object CREATOR : Parcelable.Creator<ItemsModel> {
+        override fun createFromParcel(parcel: Parcel): ItemsModel {
+            return ItemsModel(parcel)
+        }
+
+        override fun newArray(size: Int): Array<ItemsModel?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
