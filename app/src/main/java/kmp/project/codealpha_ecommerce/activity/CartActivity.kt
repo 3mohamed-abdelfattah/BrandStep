383c5f6eae81afabeec879129ed87cd7ddfdf135
MohamedMohamed <m01126165016@icloud.com>
2024-07-08 10:25:14 +0300

    Cart Activity

--- app/src/main/java/kmp/project/codealpha_ecommerce/activity/CartActivity.kt
+++ app/src/main/java/kmp/project/codealpha_ecommerce/activity/CartActivity.kt
@@ -1,14 +1,67 @@
 package kmp.project.codealpha_ecommerce.activity
 
+import ManagmentCart
 import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.project1762.Helper.ChangeNumberItemsListener
+import kmp.project.codealpha_ecommerce.Adapter.CartAdapter
 import kmp.project.codealpha_ecommerce.databinding.ActivityCartBinding
 
 class CartActivity : BaseActivity() {
+
     private lateinit var binding: ActivityCartBinding
+    private lateinit var managmentCart: ManagmentCart
+    private var tax: Double = 0.0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityCartBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+
+        managmentCart = ManagmentCart(this)
+
+        setVariable()
+        initCartList()
+        calculateCart()
+
+    }
+
+    private fun initCartList() {
+        binding.viewCart.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        binding.viewCart.adapter = CartAdapter(managmentCart.getListCart(), this, object :
+            ChangeNumberItemsListener {
+            override fun onChanged() {
+                calculateCart()
+            }
+
+        })
+        with(binding) {
+            emptyText.visibility =
+                if (managmentCart.getListCart().isEmpty()) View.VISIBLE else View.GONE
+            scrollView2.visibility =
+                if (managmentCart.getListCart().isEmpty()) View.GONE else View.VISIBLE
+        }
+    }
+
+    private fun calculateCart() {
+        val percentTax = 0.1
+        val delivery = 10.0
+        tax = Math.round((managmentCart.getTotalFee() * percentTax) * 100) / 100.0
+        val total = Math.round((managmentCart.getTotalFee() + tax + delivery) * 100) / 100.0
+        val itemTotal = Math.round(managmentCart.getTotalFee() * 100) / 100.0
+
+        with(binding) {
+            totalFeeText.text = "$ ${itemTotal}"
+            taxText.text = "$ ${tax}"
+            deleviryText.text = "$ ${delivery}"
+            totalText.text = "$ ${total}"
+        }
+    }
+
+    private fun setVariable() {
+        binding.backBtn.setOnClickListener { finish() }
     }
 }
